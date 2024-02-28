7ea31e0638652eeeeed0276f6a72afc10e79768a
TARANjee <taranjeets120@gmail.com>
2024-02-28 12:25:53 +0530

    text size changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -15,8 +15,7 @@ function Products() {
       <div className=" text-center">
         <h4>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h4>
         <p>
-          CHEMICALS  & PROCESS <span>|</span>  POWER <span>|</span>  WATER & WASTE WATER <span>|</span>  OILS &  GAS <span>|</span> PHARMA <span>|</span> SUGARS & DISTILLERIES<span>|</span>  PAPER & PULP <span>|</span>  MARINE & DEFENCE <span>|</span>  METAL & MINING <span>|</span>  FOOD & BEVERAGE <span>|</span>
-          PETROCHEMICAL & REFINERIES<span>|</span> SOLAR <span>|</span> BUILDING <span>|</span> HVAC <span>|</span> FIRE FIGHTING <span>|</span>AGRICULTURE & RESIDENTIAL
+          CHEMICALS  & PROCESS <span>|</span>  POWER <span>|</span>  WATER & WASTE WATER <span>|</span>  OILS &  GAS <span>|</span> PHARMA <span>|</span> SUGARS & DISTILLERIES <span>|</span>  PAPER & PULP <span>|</span>  MARINE & DEFENCE <span>|</span>  METAL & MINING <span>|</span>  FOOD & BEVERAGE <span>|</span> PETROCHEMICAL & REFINERIES <span>|</span> SOLAR <span>|</span> BUILDING <span>|</span> HVAC <span>|</span> FIRE FIGHTING <span>|</span> AGRICULTURE & RESIDENTIAL
          
         </p>
       </div>
